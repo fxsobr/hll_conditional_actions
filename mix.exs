1bd81c80607afd83ea972f661fb5b1d017dab426
Marcelo Ceolin <fxsobr@gmail.com>
2026-08-16 23:45:04 -0300

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HllConditionalActions.MixProject do
   def project do
     [
       app: :hll_conditional_actions,
-      version: "0.1.0",
+      version: "0.1.2",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
